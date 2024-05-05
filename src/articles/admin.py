e15e0b4fbced82f0be85c7e37d32d196472794a4
Paul Schilling <mail@paulschilling.de>
2024-05-05 19:35:55 +0200

    Customize Source admin with inlines for sitemap + rss

--- src/articles/admin.py
+++ src/articles/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, StackedInline
 
-from .models import Article, Source
+from .models import Article, RSSFeed, Sitemap, Source
 
-admin.site.register(Source)
 admin.site.register(Article)
+
+
+class SitemapInline(StackedInline):
+    model = Sitemap
+    classes = ["collapse"]
+
+
+class RSSInline(StackedInline):
+    model = RSSFeed
+    classes = ["collapse"]
+    extra = 0
+
+
+@admin.register(Source)
+class SourceAdmin(ModelAdmin):
+    inlines = (SitemapInline, RSSInline)
+    fieldsets = (
+        (
+            "Basic info",
+            {
+                "fields": [
+                    "title",
+                    "slug",
+                    "publication_type",
+                    "language",
+                    "url",
+                ]
+            }
+        ),
+    )
